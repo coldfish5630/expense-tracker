bb058f895f440e640e9f935e0a98e88862a118ae
coldfish5630 <layan.schicksal@gmail.com>
2022-06-19 11:08:01 +0900

    project init

---
+++ app.js
@@ -0,0 +1,15 @@
+if (process.env.NODE !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const port = process.env.PORT || 3000
+const { engine } = require('express-handlebars')
+
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(routes)
+
+app.listen(port, () => console.log(`expense tracker is running on ${port}`))
